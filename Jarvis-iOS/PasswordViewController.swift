77322ea11fd31849e0f43d455a00ba59e8e9a6ea
Jason Pierna <siilver777@me.com>
2017-07-13 22:33:27 +0200

    Envoi de la requête de désactivation théoriquement terminée (à tester)

--- Jarvis-iOS/PasswordViewController.swift
+++ Jarvis-iOS/PasswordViewController.swift
@@ -8,36 +8,72 @@
 
 import UIKit
 
+import PKHUD
+import Alamofire
+
 class PasswordViewController: UIViewController {
 	
-	@IBOutlet weak var addressTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 		
-		let defaults = UserDefaults.standard
-		if let address = defaults.object(forKey: "ip") as? String {
-			addressTextField.text = address
-		}
 	}
 	
-	@IBAction func saveAddress() {
-		guard let address = addressTextField.text else {
+	@IBAction func disableAlarm() {
+		
+		guard let password = passwordTextField.text else {
 			let alertController = UIAlertController(title: "Merci de remplir le champ", message: "Le champ ne doit pas être vide.", preferredStyle: .alert)
 			alertController.addAction(UIAlertAction(title: "OK", style: .default))
 			present(alertController, animated: true)
 			return
 		}
 		
+		// 1. Vérifier qu'une IP est enregistrée
+		
 		let defaults = UserDefaults.standard
-		defaults.set(address, forKey: "ip")
+		guard let address = defaults.object(forKey: "ip") as? String else {
+			let alertController = UIAlertController(title: "Adresse IP nécessaire", message: "Merci de renseigner une adresse IP dans les réglages.", preferredStyle: .alert)
+			
+			let okAction = UIAlertAction(title: "OK", style: .default) { action in
+				
+			}
+			
+			alertController.addAction(okAction)
+			present(alertController, animated: true)
+			return
+		}
 		
-		let alertController = UIAlertController(title: "IP enregistrée", message: "L'adresse " + address + " a bien été enregistrée.", preferredStyle: .alert)
-		alertController.addAction(UIAlertAction(title: "OK", style: .default))
+		let url = "http://" + address
 		
-		addressTextField.resignFirstResponder()
-		present(alertController, animated: true)
+		// 2. Démarrer le wait
+		passwordTextField.resignFirstResponder()
+		HUD.show(.labeledProgress(title: "Désactivation…", subtitle: nil))
+		
+		// 3. Envoyer la requête
+		let headers = [
+			"Content-Type" : "application/x-www-form-urlencoded"
+		]
+		
+		let parameters = [
+			"password" : password
+		]
+		
+		Alamofire.request(url, method: .post, parameters: parameters, headers: headers).responseString(completionHandler: { response in
+			switch response.result {
+			case .success(let value):
+				print(value)
+				DispatchQueue.main.async {
+					HUD.flash(.success)
+				}
+			case .failure(let error):
+				print(error)
+				DispatchQueue.main.async {
+					HUD.flash(.error)
+				}
+			}
+		})
+		
 	}
 	
 }
@@ -45,8 +81,7 @@ class PasswordViewController: UIViewController {
 extension PasswordViewController: UITextFieldDelegate {
 	
 	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-		saveAddress()
-		
+		disableAlarm()
 		return true
 	}
 }
