497cb322eec66a9772592af7a95d5b81b83b0e62
Jason Pierna <siilver777@me.com>
2017-07-13 17:36:34 +0200

    Création du projet

---
+++ Jarvis-iOS/AddressViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddressViewController.swift
+//  Jarvis-iOS
+//
+//  Created by Jason Pierna on 13/07/2017.
+//  Copyright © 2017 Jason Pierna. All rights reserved.
+//
+
+import UIKit
+
+class AddressViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+}
